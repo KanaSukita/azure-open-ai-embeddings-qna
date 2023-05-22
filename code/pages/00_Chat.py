328ae6fb4a0a8fd0ae87d435409596efad3b1dfc
KanaSukita <wez177@eng.ucsd.edu>
2023-05-22 09:15:22 +0800

    add tp-link logo

--- code/pages/00_Chat.py
+++ code/pages/00_Chat.py
@@ -14,10 +14,10 @@ def clear_chat_data():
     st.session_state['source_documents'] = []
 
 # Initialize chat history
-if 'custom_prompt' not in st.session_state:
-    st.session_state['custom_prompt'] = ""
-if 'custom_temperature' not in st.session_state:
-    st.session_state['custom_temperature'] = float(os.getenv("OPENAI_TEMPERATURE", 0.7))
+if 'custom_chat_prompt' not in st.session_state:
+    st.session_state['custom_chat_prompt'] = ""
+if 'custom_chat_temperature' not in st.session_state:
+    st.session_state['custom_chat_temperature'] = float(os.getenv("OPENAI_TEMPERATURE", 0.7))
 if 'question' not in st.session_state:
     st.session_state['question'] = None
 if 'chat_history' not in st.session_state:
@@ -26,23 +26,23 @@ if 'source_documents' not in st.session_state:
     st.session_state['source_documents'] = []
 
 # Setting
-custom_prompt_placeholder = """{summaries}
+custom_chat_prompt_placeholder = """{summaries}
     Please reply to the question using only the information present in the text above.
     Include references to the sources you used to create the answer if those are relevant ("SOURCES").
     If you can't find it, reply politely that the information is not in the knowledge base.
     Question: {question}
     Answer:"""
-custom_prompt_help = """You can configure a custom prompt by adding the variables {summaries} and {question} to the prompt.
+custom_chat_prompt_help = """You can configure a custom prompt by adding the variables {summaries} and {question} to the prompt.
 {summaries} will be replaced with the content of the documents retrieved from the VectorStore.
 {question} will be replaced with the user's question.
     """
 
-llm_helper = LLMHelper(custom_prompt=st.session_state.custom_prompt, temperature=st.session_state.custom_temperature)
+llm_helper = LLMHelper(custom_prompt=st.session_state.custom_chat_prompt, temperature=st.session_state.custom_chat_temperature)
 col1, col2, col3 = st.columns([2,2,2])
 with col3:
     with st.expander("Settings"):
-        st.slider("Temperature", min_value=0.0, max_value=1.0, step=0.1, key='custom_temperature')
-        st.text_area("Custom Prompt", key='custom_prompt', on_change=check_variables_in_prompt, placeholder= custom_prompt_placeholder,help=custom_prompt_help, height=150)
+        st.slider("Temperature", min_value=0.0, max_value=1.0, step=0.1, key='custom_chat_temperature')
+        st.text_area("Custom Prompt", key='custom_chat_prompt', on_change=check_variables_in_prompt, placeholder= custom_chat_prompt_placeholder,help=custom_chat_prompt_help, height=150)
 
 # Chat 
 st.text_input("You: ", placeholder="type your question", key="input", on_change=clear_text_input)
